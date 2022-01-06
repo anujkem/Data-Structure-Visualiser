4a43be5bc784a9e06f7e1284f2235b6658f53ad0
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2022-01-06 16:47:31 +0530

    Add Tree and Queue
    Added Basic Operations of Binary Search Tree and Queue.

--- PBL.cpp
+++ PBL.cpp
@@ -32,7 +32,7 @@ void arrayDisplay()
     clock_t tStart = clock();
     
     cout<<"\n Current Array is : \n";
-    cout<<"\n\n\t--------------------------------------------------------------------------------------------------------------------------";
+    cout<<"\n\n\t------------------------------------------------------------------------------------------------------------------------";
     cout<<"\n\n\t  ";
     
     for(int i = 0 ; i < n - 1 ; i++)
@@ -42,7 +42,7 @@ void arrayDisplay()
     
     cout<<"［"<<a[n-1]<<"］";
     
-    cout<<"\n\n\t--------------------------------------------------------------------------------------------------------------------------";
+    cout<<"\n\n\t------------------------------------------------------------------------------------------------------------------------";
     cout<<"\n\n\n\n";
 
     printf(" Time taken for Traversal : %.7fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
@@ -191,8 +191,8 @@ void traversal(struct node*ptr)
     else
     {
     cout<<"\n\n │ Current Linked List │\n\n\n";
-    cout<<"\t------------------------------------------------------------------------------------------------------------------------------";
-    cout<<"\n\n\n\t";
+    
+    cout<<"\n    ";
     
     while(ptr!=NULL)
     {
@@ -217,8 +217,8 @@ void traversal(struct node*ptr)
         ptr=ptr->next;
     }
     
-    cout<<"\n\n\n\n\t------------------------------------------------------------------------------------------------------------------------------";
-    cout<<"\n\n";
+    
+    cout<<"\n\n\n\n\n";
     
     printf(" Time taken for Traversal : %.7fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     }
@@ -444,8 +444,420 @@ void pop()
    
 }
 
+//--------------------------------------------------------Tree Functions-------------------------------------------------------//
 
 
+struct Tnode
+{
+    int info;
+    struct Tnode *left;
+    struct Tnode *right;
+    
+}*root;
+
+Tnode *Ttemp;
+
+void find(int, Tnode **, Tnode **);    
+void insert(Tnode *, Tnode *);
+void del(int);
+void case_a(Tnode *,Tnode *);
+void case_b(Tnode *,Tnode *);
+void case_c(Tnode *,Tnode *);
+void preorder(Tnode *);
+void inorder(Tnode *);
+void postorder(Tnode *);
+void display(Tnode *, int);
+
+
+
+//Find Function : Utility for Delete
+
+
+
+void find(int item, Tnode **par, Tnode **loc)
+{
+    Tnode *ptr, *ptrsave;
+    if (root == NULL)
+    {
+        *loc = NULL;
+        *par = NULL;
+        return;
+    }
+    if (item == root->info)
+    {
+        *loc = root;
+        *par = NULL;
+        return;
+    }
+    if (item < root->info)
+        ptr = root->left;
+    else
+        ptr = root->right;
+    ptrsave = root;
+    while (ptr != NULL)
+    {
+        if (item == ptr->info)
+        {
+            *loc = ptr;
+            *par = ptrsave;
+            return;
+        }
+        ptrsave = ptr;
+        if (item < ptr->info)
+            ptr = ptr->left;
+	else
+	    ptr = ptr->right;
+    }
+    *loc = NULL;
+    *par = ptrsave;
+}
+
+
+//Delete Function
+
+
+void del(int item)
+{
+    Tnode *parent, *location;
+    if (root == NULL)
+    {
+        cout<<"\n Tree is currently empty ."<<endl;
+        return;
+    }
+    find(item, &parent, &location);
+    if (location == NULL)
+    {
+        cout<<"\n Entered Item is not present in the Tree ."<<endl;
+        return;
+    }
+    if (location->left == NULL && location->right == NULL)
+        case_a(parent, location);
+    if (location->left != NULL && location->right == NULL)
+        case_b(parent, location);
+    if (location->left == NULL && location->right != NULL)
+        case_b(parent, location);
+    if (location->left != NULL && location->right != NULL)
+        case_c(parent, location);
+    free(location);
+    
+    cout<<"\n Entered Item is Deleted from the Tree.";
+    
+    cout<<"\n\n | Current BST | \n\n ";
+    display(root, 1);
+}
+
+
+
+void case_a(Tnode *par, Tnode *loc )
+{
+    if (par == NULL)
+    {
+        root = NULL;
+    }
+    else
+    {
+        if (loc == par->left)
+            par->left = NULL;
+        else
+            par->right = NULL;
+    }
+}
+
+
+
+
+void case_b(Tnode *par, Tnode *loc)
+{
+    Tnode *child;
+    if (loc->left != NULL)
+        child = loc->left;
+    else
+        child = loc->right;
+    if (par == NULL)
+    {
+        root = child;
+    }
+    else
+    {
+        if (loc == par->left)
+            par->left = child;
+        else
+            par->right = child;
+    }
+}
+
+
+void case_c(Tnode *par, Tnode *loc)
+{
+    Tnode *ptr, *ptrsave, *suc, *parsuc;
+    ptrsave = loc;
+    ptr = loc->right;
+    while (ptr->left != NULL)
+    {
+        ptrsave = ptr;
+        ptr = ptr->left;
+    }
+    suc = ptr;
+    parsuc = ptrsave;
+    if (suc->left == NULL && suc->right == NULL)
+        case_a(parsuc, suc);
+    else
+        case_b(parsuc, suc);
+    if (par == NULL)
+    {
+        root = suc;
+    }
+    else
+    {
+        if (loc == par->left)
+            par->left = suc;
+        else
+            par->right = suc;
+    }
+    suc->left = loc->left;
+    suc->right = loc->right;
+}
+ 
+
+
+void insert(Tnode *tree, Tnode *newnode)
+{
+    
+    if (root == NULL)
+    {
+        root = new Tnode;
+        root->info = newnode->info;
+        root->left = NULL;
+        root->right = NULL;
+        cout<<"\n Root Node is Added in the BST ."<<endl;
+        return;
+    }
+    if (tree->info == newnode->info)
+    {
+        cout<<"\n Entered Element is already in the tree ."<<endl;
+        return;
+    }
+    if (tree->info > newnode->info)
+    {
+        if (tree->left != NULL)
+        {
+            insert(tree->left, newnode);	
+	}
+	else
+	{
+            tree->left = newnode;
+            (tree->left)->left = NULL;
+            (tree->left)->right = NULL;
+            cout<<"\n Node is Added To Left <----."<<endl;
+            cout<<"\n | Current Tree | \n\n";
+            display(root,1);
+            return;
+        }
+    }
+    else
+    {
+        if (tree->right != NULL)
+        {
+            insert(tree->right, newnode);
+        }
+        else
+        {
+            tree->right = newnode;
+            (tree->right)->left = NULL;
+            (tree->right)->right = NULL;
+            cout<<"\n Node Added To Right ----> ."<<endl;
+            cout<<"\n\n | Current Tree | \n\n ";
+            display(root, 1);
+            return;
+        }	
+    }
+    
+}
+
+
+
+
+
+void preorder(Tnode *ptr)
+{
+    if (root == NULL)
+    {
+        cout<<"\n Tree is empty"<<endl;
+        return;
+    }
+    
+    
+    if (ptr != NULL)
+    {
+        cout<<ptr->info<<"  ";
+        preorder(ptr->left);
+        preorder(ptr->right);
+    }
+    
+}
+
+
+
+
+void inorder(Tnode *ptr)
+{
+    
+    if (root == NULL)
+    {
+        cout<<"\n Current Tree is | EMPTY |"<<endl;
+        return;
+    }
+    
+    
+    if (ptr != NULL)
+    {
+        inorder(ptr->left);
+        cout<<ptr->info<<"  ";
+        inorder(ptr->right);
+    }
+    
+}
+ 
+
+
+
+void postorder(Tnode *ptr)
+{
+    
+    if (root == NULL)
+    {
+        cout<<"\n Current Tree is | EMPTY |"<<endl;
+        return;
+    }
+    
+    
+    if (ptr != NULL)
+    {
+        postorder(ptr->left);
+        postorder(ptr->right);
+        cout<<ptr->info<<"  ";
+    }
+    
+}
+
+
+//Tree Display Function -----------------------------------------------------------------------------------------//
+
+
+void display(Tnode *ptr, int level)
+{
+    int i;
+    
+    if (ptr != NULL)
+    {
+        display(ptr->right, level+1);
+        cout<<endl;
+        
+        if (ptr == root)
+            cout<<" ●--> ";
+        else
+        {
+            for (i = 0 ; i < level ; i++)
+                cout<<"        ";
+	    }
+	    
+        cout<<"|"<<ptr->info<<"|--［";
+        
+        display(ptr->left, level+1);
+    }
+    
+    cout<<"\n";
+    return;
+    
+}
+
+
+
+//---------------------------------------------------------Queue Functions------------------------------------------------------//
+
+
+
+#define MAX 50
+
+void Qinsert();
+void Qdelete();
+void Qdisplay();
+int queue_array[50];
+int rear = - 1;
+int front = - 1;
+
+
+
+void Qinsert()
+{
+    int add_item;
+    
+    clock_t tStart = clock();
+    
+    if (rear == MAX - 1)
+    cout<<"\n Queue Overflow !!! Allocated Space in full...... \n";
+    else
+    {
+        if (front == - 1)
+        /*If queue is initially empty */
+        front = 0;
+        cout<<"\n Enter the element you want to insert in queue : ";
+        cin>>add_item;
+        rear = rear + 1;
+        queue_array[rear] = add_item;
+    }
+    
+    printf("\n\n Time Taken in Insertion : %.7fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    
+    cout<<"\n\n";
+    Qdisplay();
+} 
+
+
+
+void Qdelete()
+{
+    clock_t tStart = clock();
+    
+    if (front == - 1 || front > rear)
+    {
+        cout<<"\n Queue Underflow \n";
+        return ;
+    }
+    else
+    {
+        cout<<"\n Element deleted from queue is : | "<< queue_array[front]<<" |";
+        front = front + 1;
+    }
+    
+    
+    printf("\n\n Time Taken in Deleting Element : %.7fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    
+    cout<<"\n\n";
+    Qdisplay();
+    
+}
+
+
+
+void Qdisplay()
+{
+    clock_t tStart = clock();
+    
+    int i;
+    if (front == - 1)
+        cout<<"\n Queue is empty ";
+    else
+    {
+        cout<<"\n| Current Queue | \n\n\n\t";
+        for (i = front; i <= rear; i++)
+             cout<<"| "<<queue_array[i]<<" | <-- ";
+        cout<<"\n";
+    }
+    
+    printf("\n\n Time Taken in Traversal of Queue : %.7fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    
+} 
 
 
 //---------------------------------------------------------Main Function--------------------------------------------------------//
@@ -457,7 +869,7 @@ void pop()
 int main()
 {  
     
-    int choice,ch,a,c;
+    int choice,ch,a,c,num;
     head = NULL;
     
     
@@ -468,7 +880,7 @@ do
     cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
     
     cout<<"\n\n\n\n Choose a Data Structure To perform Operations :";
-    cout<<"\n\n 1. Stack  \n 2. Linked List \n 3. Array \n 4. Queue (X) \n 5. Tree (X) \n";
+    cout<<"\n\n 1. Stack  \n 2. Linked List \n 3. Array \n 4. Tree (BST) \n 5. Queue  \n 6. Exit";
     cout<<"\n\n Enter your choice : ";
     
     cin>>choice;
@@ -487,7 +899,7 @@ do
              cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
              
              cout<<"\n\n\n Operations ::\n";
-             cout<<"\n 1 : Push \n 2 : Pop \n 3 : Display \n 4 : Go to Main Menu \n"<<endl;
+             cout<<"\n 1 : Push \n 2 : Pop \n 3 : Visualise \n 4 : Go to Main Menu \n"<<endl;
              cout<<"\n Enter your choice of Operation : ";
              
              cin>>ch;
@@ -550,7 +962,7 @@ do
               cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
               
               cout<<"\n\n\n Operations ::\n";
-              cout<<"\n 1. Display \n 2. Insertion \n 3. Deletion \n 4. Searching \n 5. Go to Main Menu \n";
+              cout<<"\n 1. Visualise \n 2. Insert \n 3. Delete \n 4. Search\n 5. Go to Main Menu \n";
               cout<<"\n\n Enter your choice of Operation : ";
               cin>>ch;
               
@@ -609,7 +1021,7 @@ do
               cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
               
               cout<<"\n\n\n Operations ::\n";
-              cout<<"\n 1. Create new Array \n 2. Display \n 3. Insert \n 4. Delete \n 5. Go to Main Menu \n";
+              cout<<"\n 1. Create new Array \n 2. Visualise \n 3. Insert \n 4. Delete \n 5. Go to Main Menu \n";
               cout<<"\n\n Enter your choice of Operation : ";
               cin>>ch;
               
@@ -654,21 +1066,144 @@ do
         cout << "\033[2J\033[1;1H";      
         break;
         
+        
+    //Tree
+        
     case 4: cout << "\033[2J\033[1;1H"; 
-            cout<<"\n Ruko Zara Sabar Karo......\n\n Nahin Bana ye function abhi....\n Cross Nai Dikha....\n\n Le Exit Ho tu ab yahan se......";
-            exit(1);
-            break;
+            
+            
+            do
+            {
+              cout << "\033[2J\033[1;1H";
+              cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
+              cout<<"\n\t\t\t\t\t   ||            Tree Data Structure Menu           ||";
+              cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
+              
+              cout<<"\n\n\n Operations ::\n";
+              cout<<"\n 1. Insert \n 2. Delete  \n 3. Inorder \n 4. Preorder \n 5. Postorder \n 6. Visualise \n 7. Go to Main Menu";
+              cout<<"\n\n Enter your choice of Operation : ";
+              cin>>ch;
+              
+              if(ch == 7)
+               break;
+               
+              switch(ch)
+              {
+                  case 1: cout << "\033[2J\033[1;1H";
+                          Ttemp = new Tnode;
+                          cout<<"\n Enter the number to be inserted : ";
+	                      cin>>Ttemp->info;
+                          insert(root, Ttemp);
+                          break;
+                          
+                  case 2: cout << "\033[2J\033[1;1H";
+                  
+                          if (root == NULL)
+                          {
+                            cout<<"\n Tree is empty, nothing to delete ."<<endl;
+                            continue;
+                          }
+                          
+                          
+                          cout<<"\n Enter the number to be deleted : ";
+                          cin>>num;
+                          del(num);
+                          break;
+                   
+                   case 3: cout << "\033[2J\033[1;1H";
+                           cout<<"\n Inorder Traversal of BST : ";
+                           inorder(root);
+                           break;
+                    
+                    
+                   case 4: cout << "\033[2J\033[1;1H";
+                           cout<<"\n Preorder Traversal of BST : ";
+                           preorder(root);
+                           break;
+                   
+                   case 5: cout << "\033[2J\033[1;1H";
+                           cout<<"\n Postorder Trversal of BST : ";
+                           postorder(root);
+                           break;
+                    
+                   case 6: cout << "\033[2J\033[1;1H";
+                           cout<<"\n\n\n | Current BST | \n\n\n ";
+                           display(root,1);
+                           cout<<"\n\n";
+                           
+                           break;
+                
+                   case 7: exit(1);
+                   
+                   default: cout<<"\nWrong Choice!!!";
+                  
+              }
+            
+            cout<<"\n\n\n|------------------------------------------------------------------|";
+            cout<<"\n\n 1 : To Go To Tree Menu";
+            cout<<"\n 2 : To Go Back To Main Menu ";
+            cout<<"\n\n Enter your Choice : ";
+            cin>>c;
+              
+            }while(c==1);
+            
+        cout << "\033[2J\033[1;1H";      
+        break;
+              
+    
+    //Queue         
+              
         
     case 5: cout << "\033[2J\033[1;1H"; 
-            cout<<"\n Ruko Zara Sabar Karo......\n\n Nahin Bana ye function abhi....\n Cross Nai Dikha....\n\n Le Exit Ho tu ab yahan se......";
-            exit(1);
-            break;
+            
+            
+            do
+            {
+              cout << "\033[2J\033[1;1H";
+              cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
+              cout<<"\n\t\t\t\t\t   ||            Queue Data Structure Menu          ||";
+              cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
+              
+              cout<<"\n\n\n Operations ::\n";
+              cout<<"\n 1. Insert \n 2. Delete  \n 3. Visualise \n 4. Go to Main Menu";
+              cout<<"\n\n Enter your choice of Operation : ";
+              cin>>ch;
+              
+              if(ch == 4)
+               break;
+               
+              switch(ch)
+              {
+                case 1: cout << "\033[2J\033[1;1H";
+                        Qinsert();
+                        break;
+                        
+                case 2: cout << "\033[2J\033[1;1H";
+                        Qdelete();
+                        break;
+                        
+                case 3: cout << "\033[2J\033[1;1H";
+                        Qdisplay();
+                        break;
+                        
+                default: cout<<"\nWrong Choice";
+              }
+             cout<<"\n\n\n|------------------------------------------------------------------|";
+             cout<<"\n\n 1 : To Go To Queue Menu";
+             cout<<"\n 2 : Go Back To Main Menu ";
+             cout<<"\n\n Enter your Choice : ";
+             cin>>c;
+        
+            }while(c==1);
+                 
+       cout << "\033[2J\033[1;1H";     
+       break;
             
     //Exit
     
     case 6: cout << "\033[2J\033[1;1H";  
             cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
-            cout<<"\n\t\t\t\t\t   ||    Thanks for Using.......     Bye Bye....    ||";
+            cout<<"\n\t\t\t\t\t   ||................Thanks for Using...............||";
             cout<<"\n\t\t\t\t\t   ||-----------------------------------------------||";
             exit(0);
               
@@ -681,8 +1216,5 @@ do
 
     return 0;
 }
-
-
-
 
 
